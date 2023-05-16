50d7480251bd443c95577d52d631d2e314665779
weijianshi <shiweijian0818@gmail.com>
2023-05-16 15:25:23 -0700

    scrapy update

--- ourfirstscraper/ourfirstscraper/spiders/redditbot.py
+++ ourfirstscraper/ourfirstscraper/spiders/redditbot.py
@@ -7,4 +7,21 @@ class RedditbotSpider(scrapy.Spider):
     start_urls = ["https://www.reddit.com/r/gameofthrones/"]
 
     def parse(self, response):
-        pass
+        # Extracting the content using css selectors
+        titles = response.css('.title.may-blank::text').extract()
+        votes = response.css('.score.unvoted::text').extract()
+        times = response.css('time::attr(title)').extract()
+        comments = response.css('.comments::text').extract()
+
+        # Give the extracted content row wise
+        for item in zip(titles, votes, times, comments):
+            # create a dictionary to store the scraped info
+            scraped_info = {
+                'title': item[0],
+                'vote': item[1],
+                'created_at': item[2],
+                'comments': item[3],
+            }
+
+            # yield or give the scraped info to scrapy
+            yield scraped_info
